7c7efed04d45c7b5b78d9fc61d18dfa66fe45984
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-08 20:55:11 +0530

    added iframe

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import "../stylesheets/PlayingVideo.css"
+import { useParams } from 'react-router-dom'
 const PlayingVideo = () => {
+  let id = useParams().id;
+  console.log(id);
   return (
-    <div>jii</div>
+    <div id="video-playing">
+      <div id="nowVideo">
+        <div id="player">
+        <iframe  src={`https://www.youtube.com/embed/${id}?si=_FWj7NrAbNWOZvur`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+        <div id="video-info">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
